9489302f366a2f61cf353f43f2ac1bb5f744b34f
Spin Rodriguez <favorspin@users.noreply.github.com>
2024-09-12 15:21:30 -0500

    updated help, fixed labeling and removed entropy/seed display

--- gen.py
+++ gen.py
@@ -3,10 +3,29 @@ from mnemonic import Mnemonic
 from eth_account import Account
 
 def usage():
-    print("Usage: generator [-a] [-s 128|160|192|224|256] [-i] [INPUT]")
+    print("Usage: python gen.py [-a] [-s 128|160|192|224|256] [-i] [INPUT]")
+    print("For more info, see python gen.py --help")
 
 def help():
-    print("TODO: Display help")
+    print("This is a simple script to generate mnemonic phrases and ETH address. By default,")
+    print("the script will generate a random 128 bit mnemonic phrase that can be used to")
+    print("generate wallet address on several networks.\n")
+    print("Simple usage example:")
+    print("\tpython gen.py")
+    print("> Mnemonic: eager supply congress gaze daughter mix issue vacant practice ivory treat vibrant\n")
+    print("Example address output:")
+    print("\tpython gen.py -a")
+    print("> Mnemonic   : pistol three track razor chicken topple globe sustain blade dignity taxi potato")
+    print("> ETH Address: 0x6e30Fd6b04cdEa38Ef3ee8a8F7247C07f1e6f873\n")
+    print("The script can also generate an address from an existing mnemonic by specifiying the -i flag")
+    print("followed by the mnemonic as follows:")
+    print("\t python gen.py -a -i future crunch diamond near dry soul tomorrow under talent ready victory grit")
+    print("> Mnemonic   : future crunch diamond near dry soul tomorrow under talent ready victory grit")
+    print("> ETH Address: 0x68a0d7Ffba34A54A272C6E09cf54A8A0622C0368\n")
+    print("Strength of the mnemonic can also be set with the -s flag as follows. By default this is set to 128 bits.")
+    print("\t python gen.py -s 256")
+    print("> voyage upgrade story hundred toe slice hurt mandate cannon wide wool help moon custom speed glass lemon wear pond sea canvas income blanket wisdom")
+
 
 
 def main(argv):
@@ -46,23 +65,17 @@ def main(argv):
     entropy = mnemo.to_entropy(words)
     seed = mnemo.to_seed(words)
 
-    print('Mnemonic:')
-    print(f'\t {words}\n')
-    print('Entropy:')
-    print(f'\t {entropy.hex()}\n')
-    print('Seed:')
-    print(f'\t {seed.hex()}\n')
-
-    ##address stuff:
+    print(f'Mnemonic   : {words}')
+    # print(f'Entropy    : {entropy.hex()}')
+    # print(f'Seed       : {seed.hex()}')
 
     if create_address:
 
-        print('Addresses:')
         # eth address
         Account.enable_unaudited_hdwallet_features()
         acct = Account.from_mnemonic(words)
 
-        print(f'\tEth: {acct.address}')
+        print(f'ETH Address: {acct.address}')
 
 
     print("WRITE DOWN YOUR MNEMONIC. STORE IN A SAFE PLACE.")
